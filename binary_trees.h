9422a26dca09ce246139b65f5df1f1557178381b
djso89 <danielso89@gmail.com>
2020-07-25 02:27:44 -0700

    fixed include guard

--- binary_trees.h
+++ binary_trees.h
@@ -1,5 +1,5 @@
-#ifndef BINARY_TREES
-#define BINARY_TREES
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -23,5 +23,6 @@ typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
 void binary_tree_print(const binary_tree_t *);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
 #endif /* binary_trees*/
