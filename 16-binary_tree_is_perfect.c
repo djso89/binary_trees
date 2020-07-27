19a5a3d67685824a3a0cd8524b069d9d0f968b17
djso89 <danielso89@gmail.com>
2020-07-26 23:36:43 -0700

    modified if when NULL is passed

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -30,18 +30,19 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	size_t l, r;
 
+	if (!tree)
+		return (0);
 	if (!tree->left && !tree->right)
 		return (1);
-	if (tree == NULL || (tree->left == NULL || tree->right == NULL))
-		return (0);
 	l = binary_tree_height(tree->left);
 	r = binary_tree_height(tree->right);
 
-	if (l == r)
+	if (l != r)
 	{
-		if (binary_tree_is_perfect(tree->left) &&
-		    binary_tree_is_perfect(tree->right))
-			return (1);
+		return (0)
 	}
+	if (binary_tree_is_perfect(tree->left) &&
+	    binary_tree_is_perfect(tree->right))
+		return (1);
 	return (0);
 }
