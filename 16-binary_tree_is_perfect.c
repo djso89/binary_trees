8e827802466061b590de9476fd7f7969d24613b2
djso89 <danielso89@gmail.com>
2020-07-26 23:22:27 -0700

    modified if

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -32,7 +32,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 	if (!tree->left && !tree->right)
 		return (1);
-	if (!tree || (!tree->left || !tree->right))
+	if (!tree || (tree->left == NULL || tree->right == NULL))
 		return (0);
 	l = binary_tree_is_perfect(tree->left);
 	r = binary_tree_is_perfect(tree->right);
